c1d18de15d3042d5999039c535fccd906ff7252e
unknown <isaacjamesaaku@gmail.com>
2024-09-03 14:53:42 +0100

    complete working app

---
+++ convex/auth.config.ts
@@ -0,0 +1,11 @@
+
+const authConfig ={
+  providers: [
+    {
+      domain: "https://correct-silkworm-74.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
+
+export default authConfig
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images:{
+        remotePatterns:[
+            {hostname:"beaming-mouse-68.convex.cloud"},
+        ]
+    }
+};
 
 export default nextConfig;
